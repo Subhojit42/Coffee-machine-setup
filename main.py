edaca4c171917e1fde66644c07d1a436ac1c2cad
SUBHOJIT KAYAL <71116205+Subhojit42@users.noreply.github.com>
2021-07-21 02:16:35 +0530

    code

---
+++ main.py
@@ -0,0 +1,27 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+
+is_on = True
+
+money_machine.report()
+coffee_maker.report()
+
+while is_on:
+    options = Menu.get_items()
+    choice = input(f"What should you like? : {options}")
+    if choice== "off":
+        is_on =False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = Menu.find_drink(choice)
+        if (coffee_maker.is_resource_sufficient(drink)):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
+
